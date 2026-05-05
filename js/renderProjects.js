81238386acb8c8be08ad94d048231c9c18fe8034
evgeniia-korolova <jkod@ukr.net>
2026-05-05 16:55:29 +0300

    feat: style project-cards

--- js/renderProjects.js
+++ js/renderProjects.js
@@ -1,17 +1,19 @@
 const renderProjects = (data, containerSelector) => {
-    const container = document.querySelector(containerSelector);    
-    
-    container.innerHTML = '';
+  const container = document.querySelector(containerSelector);
 
-    data.forEach((project, index) => {        
-        const tagsHtml = project.tags
-            .map(tag => `<li class="tags-item">${tag}</li>`)
-            .join('');
+  container.innerHTML = '';
 
-        
-        const cardHtml = `
-            <div class="card card-animate" style="animation-delay: ${index * 0.1}s" data-id="${project.id}">
-                <img src="${project.image}" alt="${project.title}" class="card-image" />
+  data.forEach((project, index) => {
+    const tagsHtml = project.tags
+      .map((tag) => `<li class="tags-item">${tag}</li>`)
+      .join('');
+
+    const cardHtml = `
+            <div class="card card-animate" style="animation-delay: ${
+              index * 0.1}s" data-id="${project.id}">
+                <div class="image-wrapper">
+                    <img src="${project.image}" alt="${project.title}" class="card-image" />
+                </div>
                 <div class="card-info">
                     <h3 class="card-title accent">${project.title}</h3>
                     <ul class="tags">
@@ -26,15 +28,21 @@ const renderProjects = (data, containerSelector) => {
                     </button>
                     <div class="card-overlay">
                         <div class="overlay-content">
-                            <p class="card-description">${project.description}</p>
+                            <p class="card-description">${
+                              project.description
+                            }</p>
                         </div>
                     </div>
                     <div class="card-buttons">
-                        <a href="${project.links.code}" class="project-link" target="_blank">
+                        <a href="${
+                          project.links.code
+                        }" class="project-link" target="_blank">
                             <img src="./images/icons/code.svg" alt="code-icon" class="project-link-icon" />
                             <span class="project-link-text">Code</span>
                         </a>
-                        <a href="${project.links.demo}" class="project-link" target="_blank">
+                        <a href="${
+                          project.links.demo
+                        }" class="project-link" target="_blank">
                             <img src="./images/icons/demo.svg" alt="demo-svg" class="project-link-icon" />
                             <span class="project-link-text">Demo</span>
                         </a>
@@ -43,8 +51,8 @@ const renderProjects = (data, containerSelector) => {
             </div>
         `;
 
-        container.insertAdjacentHTML('beforeend', cardHtml);
-    });
+    container.insertAdjacentHTML('beforeend', cardHtml);
+  });
 };
 
 export default renderProjects;
